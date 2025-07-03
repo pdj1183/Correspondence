21f20f07a47bfb4842a442d83f96456577785674
Phillip Johnson <pdj1183@gmail.com>
2025-07-02 18:07:32 -0600

    Initial Commit

---
+++ Correspondence/CorrespondenceApp.swift
@@ -0,0 +1,32 @@
+//
+//  CorrespondenceApp.swift
+//  Correspondence
+//
+//  Created by Phillip Johnson on 7/2/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct CorrespondenceApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ CorrespondenceTests/CorrespondenceTests.swift
@@ -0,0 +1,17 @@
+//
+//  CorrespondenceTests.swift
+//  CorrespondenceTests
+//
+//  Created by Phillip Johnson on 7/2/25.
+//
+
+import Testing
+@testable import Correspondence
+
+struct CorrespondenceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
